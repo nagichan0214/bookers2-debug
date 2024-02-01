000b43abd3fa33c7cdecd2a8e288047fdb5f13e1
nagichan0214 <ec2-user@ip-172-31-15-20.ap-northeast-1.compute.internal>
2024-02-01 07:04:32 +0000

    finish

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,42 @@
+class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:update]
+
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+    @book = Book.new
+  end
+
+  def index
+    @users = User.all
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def edit
+    is_matching_login_user
+    @user = User.find(params[:id])
+  end
+
+  def update
+    is_matching_login_user
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "You have updated user successfully."
+    else
+      render "edit"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+  def is_matching_login_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
+  end
+end
