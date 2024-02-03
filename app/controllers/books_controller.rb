c54ae955e1aa6c25a195d39c78b75d1f4acc86c5
nagichan0214 <ec2-user@ip-172-31-15-20.ap-northeast-1.compute.internal>
2024-02-03 09:54:59 +0000

    favorite,comment finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,7 @@ before_action :is_matching_login_user, only: [:edit, :update]
     @book = Book.find(params[:id])
    @user = @book.user
    @newbook = Book.new
+   @book_comment = BookComment.new
   end
 
   def index
