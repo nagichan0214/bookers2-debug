3211b2fdb5740e67c781c7aba27b1585e37e72a8
nagichan0214 <ec2-user@ip-172-31-15-20.ap-northeast-1.compute.internal>
2024-02-06 10:04:04 +0000

    search little

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,6 +3,7 @@ before_action :authenticate_user!
 
 def search
   @range = params[:range]
+  @word = params[:word]
   
   if @range == "User"
     @users = User.looks(params[:search], params[:word])
