5064346bc1a23373a531197a6eb9cc6b89c59b6f
nagichan0214 <ec2-user@ip-172-31-15-20.ap-northeast-1.compute.internal>
2024-02-08 11:40:23 +0000

    applied6 finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -29,7 +29,7 @@ before_action :is_matching_login_user, only: [:edit, :update]
   end
 
   def update
-    
+
     @book = Book.find(params[:id])
     if @book.update(book_params)
       redirect_to book_path(@book), notice: "You have updated book successfully."
